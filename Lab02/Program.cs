0c0f3c997b32f965704c2c87c7bf175152823760
jirarojieow-ai <jiraroj.ieow@bumail.net>
2026-08-24 16:23:23 +0700

    Add Lab02

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace Lab02
+﻿/*
+ * Student ID :
+ * Name       :
+ * Section    :
+ * No.        :
+ * Course     : GI113 Computer Programming (GI
+ */
+
+namespace Lab02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string bossName = "Kirin";
+            char rank = 'S';
+            int level = 7;
+            int maxHp = 240;
+            int currentHp = 175;
+            float attackPower = 42.5f;
+            double critMultiplier = 1.75;
+            bool isBoss = true;
+            
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine();
+
+            int hpPercent = currentHp * 100 / maxHp;
+            Console.WriteLine($"HP Percent: {hpPercent}%");
+            Console.WriteLine();
+            Console.WriteLine("Kirin takes 60 damage!");
+            Console.WriteLine();
+            Console.WriteLine($"===== BOSS STATUS: AFTER DAMAGE =====");
+            Console.WriteLine($"HP: {currentHp} / {maxHp}");
+            Console.WriteLine($"HP Percent: 47%");
         }
     }
 }
